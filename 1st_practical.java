7100c107a491fc791e49e35a245dd440c0255868
yogender-316 <113343189+yogender-316@users.noreply.github.com>
2022-11-30 14:17:17 +0530

    final_commit

--- 1st_practical.java
+++ 1st_practical.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class 1st_practical {
     public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("This is 1st Practical");
         
     }
 }
